90943f66c3004f13e20076b4e98f56cbac8a223c
hoangthang3010 <thangminhhoang98@gmail.com>
2022-06-11 00:11:15 +0700

    update

--- adidaphat-4.0/src/components/HelloWorld.vue
+++ adidaphat-4.0/src/components/HelloWorld.vue
@@ -1,113 +1,40 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <img src="../assets/image.png" alt="" @click="click">
+    <audio id="yourAudioTag" controls style="display: none">
+      <source src="../assets/ting.mp3" type="audio/ogg">
+      <source id="" src="../assets/ting.mp3" type="audio/mpeg">
+    </audio>
+    <img src="../assets/image2.png" alt="" @click="click2">
+    <audio id="yourAudioTag1" controls style="display: none">
+      <source src="../assets/tiengchuongchua_3.mp3" type="audio/ogg">
+      <source id="" src="../assets/tiengchuongchua_3.mp3" type="audio/mpeg">
+    </audio>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      // click: true;
+    }
+  },
+  methods: {
+    click () {
+      document.getElementById('yourAudioTag').currentTime = 0
+      document.getElementById('yourAudioTag').play()
+      console.log(1234)
+    },
+    click2 () {
+      document.getElementById('yourAudioTag1').currentTime = 0
+      document.getElementById('yourAudioTag1').play()
+      console.log(1234)
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
